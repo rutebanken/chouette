5a43f2fe5f211f73c63ffd0f894e8bacba154ff4
vincent <vincent.paturet@entur.org>
2020-10-23 12:27:38 +0200

    Improve log messages

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexValidationCommand.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexValidationCommand.java
@@ -40,7 +40,7 @@ public class NetexValidationCommand implements Command, Constant {
             log.info(Color.MAGENTA + "Profile validation finished " + fileName + " " + monitor.stop() + Color.NORMAL);
         }
         if (result == ERROR) {
-            log.info("NeTEx validation failed for file:" + fileName);
+            log.info("NeTEx validation failed for file: " + fileName);
             reporter.addFileErrorInReport(context, fileName,
                     ActionReporter.FILE_ERROR_CODE.INVALID_FORMAT, "Netex compliance failed");
             if (!reporter.hasActionError(context))
